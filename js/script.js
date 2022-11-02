7da2220b713596838c5c32471590bf1a3953bef1
FabrizioMisseri <misserifabrizio@gmail.com>
2022-11-02 12:28:54 +0100

    add mark-up header

--- js/script.js
+++ js/script.js
@@ -1,7 +1,4 @@
-// Oggi iniziamo a realizzare Boolzapp - a (not very) innovative messaging platform ;)
-// Il compito è di svolgere la milestone 1 del brief che vi invio in allegato:
+// CONSEGNA
 // -> Replica della grafica con la possibilità di avere messaggi scritti dall’utente (verdi) e dall’interlocutore (bianco) assegnando due classi CSS diverse
 // -> Visualizzazione dinamica della lista contatti: tramite la direttiva v-for, visualizzare nome e immagine di ogni contatto.
-// Consigli del giorno:
-// - scegliete con attenzione gli strumenti che utilizzerete;
-// - tenete in mente le funzionalità che dovranno essere implementate nei giorni successive. La struttura realizzata mi faciliterà rendere il tutto dinamico? Forse mi serve una classe per distinguere i messaggi inviati da quelli ricevuti?
+// Forse mi serve una classe per distinguere i messaggi inviati da quelli ricevuti?
