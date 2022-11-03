7116b96f7e4117e99030d02a0ff61f18ff60d700
FabrizioMisseri <misserifabrizio@gmail.com>
2022-11-03 15:45:56 +0100

    add date in contacts-list

--- js/script.js
+++ js/script.js
@@ -1,16 +1,10 @@
-// CONSEGNA
-// -> Replica della grafica con la possibilità di avere messaggi scritti dall’utente (verdi) e dall’interlocutore (bianco) assegnando due classi CSS diverse
-// -> Visualizzazione dinamica della lista contatti: tramite la direttiva v-for, visualizzare nome e immagine di ogni contatto.
-// Forse mi serve una classe per distinguere i messaggi inviati da quelli ricevuti?
 
 const {createApp} = Vue;
 
 const dt = luxon.DateTime;
 
 createApp({
-
     
-
     data(){
         return{
 
